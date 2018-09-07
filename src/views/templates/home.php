00acb1bc624477aedca5d27610a22b9ab5728dec
Karl Mark Balagtey <karl.balagtey@gmail.com>
2018-09-08 00:27:37 +0100

    Move search to split view in the layout

--- src/views/templates/home.php
+++ src/views/templates/home.php
@@ -3,16 +3,18 @@
 <section class="bg-secondary">
 	<section class="container py-5">
 		<div class="row py-4">
-			<div class="col text-dark"> 
+			<div class="col-12 col-md-6 text-dark"> 
 				<h1 id="b-page-title"><?= $page->getTitle() ?></h1>
 
 				<div>
 					<?= $chunk('text', 'standfirst') ?>
 				</div>
+			</div>
 
-		        <form class="form-inline my-2 my-lg-0">
-		            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-		            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+			<div class="col-12 col-md-6 text-dark">
+		        <form class="form my-2 my-lg-0 w-100" style="max-width: 350px; float: right;">
+		            <input class="form-control mr-sm-2 mb-2" type="search" placeholder="Search" aria-label="Search">
+		            <button class="btn btn-outline-primary my-2 my-sm-0 float-right" type="submit">Search</button>
 		        </form>
 			</div>
 		</div>
